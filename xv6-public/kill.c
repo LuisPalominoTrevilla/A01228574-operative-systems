1e00f5b4fa118a8aa797ef7691b7890fa11ef5c7
Luis Palomino Trevilla <luispalominot3@gmail.com>
2020-03-04 03:03:37 +0000

    Practica 3

--- xv6-public/kill.c
+++ xv6-public/kill.c
@@ -11,7 +11,12 @@ main(int argc, char **argv)
     printf(2, "usage: kill pid...\n");
     exit();
   }
-  for(i=1; i<argc; i++)
-    kill(atoi(argv[i]));
+  for(i=1; i<argc; i++) {
+    if (atoi(argv[i]) != 1) {
+        kill(atoi(argv[i]));
+    } else {
+        printf(2, "sorry, can't kill process init\n");
+    }
+  }
   exit();
 }
